4b920eef00ead3f5b519882591f31ecee5c9c7dc
Ajay Singh Chauhan <107171838+AjaySinghChauhan85@users.noreply.github.com>
2026-06-30 18:44:26 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,22 +1,39 @@
-# Variable for EC2 instance type — defines the hardware size of the virtual machine
-variable "ec2_instance-type" {
-  default = "t2.micro" # Free-tier eligible instance type (1 vCPU, 1 GB RAM)
-  type    = string     # Value must be a string
-}
-
-# Variable for the root EBS volume size in GB
-variable "ec2_default_root_storage" {
-  default = 15   # Default root disk size is 15 GB
-  type    = number # Value must be a number
-}
-
-# Variable for the Amazon Machine Image (AMI) ID — defines the OS for the EC2 instance
-variable "ec2_ami_id" {
-  default = "ami-0a5c3558529277641" # Amazon Linux 2 AMI in us-east-1
-  type    = string                  # Value must be a string
-}
-
 variable "env" {
-    default = "dev"
-    type = string
+  description = "This is Environment for my infra"
+  type        = string
+}
+
+variable "bucket_name" {
+  description = "This is the bucket name for my infra"
+  type        = string
+}
+
+variable "instance_count" {
+  description = "This is a number of ec2 instance"
+  type        = number
+}
+
+variable "instance_type" {
+  description = "This is ec2 instance type"
+  type        = string
+}
+
+variable "ec2_ami_id" {
+  description = "This is ec2 instance ami id"
+}
+
+variable "ec2_default_root_storage" {
+  description = "The root stroage volume for ec2 instance"
+  type        = number
+}
+
+variable "hash_key" {
+  description = "The hash id for dynamodb "
+  type        = string
+
+}
+
+variable "terra_dynamodb_table" {
+  description = "This is the  dynamodb name "
+  type        = string
 }
